ceb7466ac3abd372b5f422bb761f55b0473df976
taniiisshhhhh <taniiisshhhhh@gmail.com>
2025-03-08 23:39:56 +0530

    WIP: Commit changes before merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ArunImage from './assets/Arun.jpeg';
 import imagecss from './css/App.css'
 import Navbar from './Navbar'
+
 export default function App() {
   return (
     <div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,19 @@
-
-import './css/Navbar.css'
+import "./css/Navbar.css";
 function name() {
-    return(
-        <>
-        <nav>
-
-        </nav>
-        </>
-    )
+  return (
+    <>
+      <nav>
+        <div id="blacktop">
+          <li>
+            Sign up and get 20% off to your first order.{" "}
+            <a href="X">Sign Up Now</a>
+          </li>
+        </div>
+        <div className="Navbar">
+            
+        </div>
+      </nav>
+    </>
+  );
 }
 export default name;
